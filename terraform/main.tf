4be072a7f3df8704f3df2544a80d020a4135f919
Pieter Van der Vyver <pieter@ieee.org>
2026-03-25 15:29:03 +0200

    ci: allow apply on manual workflow trigger

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "neal-street-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
